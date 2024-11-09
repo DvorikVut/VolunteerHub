abcd5de27b3623e49134c88e0b66c51ff0bb20ca
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-11-09 20:51:06 +0100

    make methods static

--- src/main/java/my/pastebin/Logger/MyLogger.java
+++ src/main/java/my/pastebin/Logger/MyLogger.java
@@ -8,26 +8,25 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyLogger {
-
     private static final Logger logger = LoggerFactory.getLogger(MyLogger.class);
 
-    public void logInfo(String message) {
+    public static void logInfo(String message) {
         logger.info(message);
     }
 
-    public void logWarn(String message) {
+    public static void logWarn(String message) {
         logger.warn(message);
     }
 
-    public void logError(String message, Throwable throwable) {
+    public static void logError(String message, Throwable throwable) {
         logger.error(message, throwable);
     }
 
-    public void logRequest(String method, String url) {
+    public static void logRequest(String method, String url) {
         logger.info("Received request: Method = {}, URL = {}", method, url);
     }
 
-    public void logResponse(String url, int statusCode, long executionTime) {
+    public static void logResponse(String url, int statusCode, long executionTime) {
         logger.info("Response for URL = {}: Status = {}, Execution Time = {}ms", url, statusCode, executionTime);
     }
 }
