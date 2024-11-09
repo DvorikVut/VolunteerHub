c25256fbba740cfcbec4e60ec56b94f76fa41836
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-11-09 21:35:03 +0100

    small refactoring

--- src/main/java/my/pastebin/Logger/RequestLoggerInterceptor.java
+++ src/main/java/my/pastebin/Logger/RequestLoggerInterceptor.java
@@ -9,28 +9,20 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class RequestLoggerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // Логирование информации о запросе перед обработкой контроллером
         String method = request.getMethod();
         String url = request.getRequestURL().toString();
-
-        // Сохранение времени начала выполнения
         request.setAttribute("startTime", System.currentTimeMillis());
-
         MyLogger.logRequest(method, url);
-
-        // Продолжить выполнение
         return true;
     }
 
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        // Логирование информации о завершении запроса
         String url = request.getRequestURL().toString();
         int status = response.getStatus();
         long startTime = (Long) request.getAttribute("startTime");
         long executionTime = System.currentTimeMillis() - startTime;
-
         MyLogger.logResponse(url, status, executionTime);
     }
 }
