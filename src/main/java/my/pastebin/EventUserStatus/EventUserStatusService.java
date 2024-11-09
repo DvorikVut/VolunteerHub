90a1b30ab3d7605eb371fd4f74c32c6278918a82
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-11-09 21:34:47 +0100

    Add comments

--- src/main/java/my/pastebin/EventUserStatus/EventUserStatusService.java
+++ src/main/java/my/pastebin/EventUserStatus/EventUserStatusService.java
@@ -7,6 +7,7 @@ import my.pastebin.Event.EventService;
 import my.pastebin.Exceptions.BadRequestException;
 import my.pastebin.Exceptions.NotAuthorizedException;
 import my.pastebin.Exceptions.ResourceNotFoundException;
+import my.pastebin.Logger.MyLogger;
 import my.pastebin.User.Role;
 import my.pastebin.User.User;
 import my.pastebin.User.UserService;
@@ -24,26 +25,51 @@ public class EventUserStatusService {
     private final EventService eventService;
     private final UserService userService;
 
-    public void changeStatus(Long eventId, Long userId, Status status) throws EntityNotFoundException  {
+    /**
+     * Changes the status of a user for a specific event.
+     *
+     * @param eventId the ID of the event
+     * @param userToChangeId the ID of the user
+     * @param status the new status to set
+     * @throws EntityNotFoundException if the user is not registered for the event
+     * @throws AccessDeniedException if the current user is not authorized to change the status
+     */
+    public void changeStatus(Long eventId, Long userToChangeId, Status status) throws EntityNotFoundException {
         User user = userService.getCurrentUser();
-            if(!(user.getRole().equals(Role.ADMIN) || user.equals(eventService.getById(eventId).getCreator()))) {
+        if (!(checkIfUserRoleIs(user,Role.ADMIN) || isUserIsCreatorOfEvent(eventId, user))) {
+            MyLogger.logInfo("EXCEPTION: User " + user.getUsername() + " is not authorized to change status of user " +
+                    userService.getUserById(userToChangeId).getUsername() + " in event " + eventService.getById(eventId).getName());
             throw new AccessDeniedException("Only creator can change status");
         }
 
-        EventUserStatus eventUserStatus = eventUserStatusRepo.findByEventIdAndUserId(eventId, userId);
-        if(eventUserStatus == null) throw new EntityNotFoundException("User is not registered for this event");
+        EventUserStatus eventUserStatus = eventUserStatusRepo.findByEventIdAndUserId(eventId, userToChangeId);
+        if (eventUserStatus == null) {
+            MyLogger.logInfo("EXCEPTION: User " + userService.getUserById(userToChangeId).getUsername() +
+                    " is not registered for event " + eventService.getById(eventId).getName());
+            throw new EntityNotFoundException("User is not registered for this event");
+        }
 
         eventUserStatus.setStatus(status);
         eventUserStatusRepo.save(eventUserStatus);
     }
-    public void registerUserForEvent(Long eventId){
+
+    /**
+     * Registers the current user for a specific event.
+     *
+     * @param eventId the ID of the event
+     * @throws ResourceNotFoundException if the event does not exist
+     * @throws BadRequestException if the event is already full
+     */
+    public void registerUserForEvent(Long eventId) {
         Event event = eventService.getById(eventId);
 
-        if(event == null) {
+        if (event == null) {
+            MyLogger.logInfo("EXCEPTION: Event with ID " + eventId + " not found");
             throw new ResourceNotFoundException("Event not found");
         }
 
-        if(event.getCapacity() <= getNumberOfConfirmedUsers(eventId)) {
+        if (event.getCapacity() <= getNumberOfConfirmedUsers(eventId)) {
+            MyLogger.logInfo("EXCEPTION: Event " + event.getName() + " is full");
             throw new BadRequestException("Event is full");
         }
 
@@ -53,33 +79,83 @@ public class EventUserStatusService {
         eventUserStatus.setStatus(Status.REGISTERED);
         eventUserStatusRepo.save(eventUserStatus);
     }
+
+    /**
+     * Deletes a user from a specific event.
+     *
+     * @param eventId the ID of the event
+     * @param userId the ID of the user
+     * @throws NotAuthorizedException if the current user is not authorized to delete the user
+     * @throws ResourceNotFoundException if the user is not registered for the event
+     */
     public void deleteUserFromEvent(Long eventId, Long userId) {
-
         Event event = eventService.getById(eventId);
-        User creator = event.getCreator();
+        User currentUser = userService.getCurrentUser();
 
-        if(!(creator.equals(userService.getCurrentUser()) || userService.getCurrentUser().getRole().equals(Role.ADMIN) || userService.getUserById(userId).equals(userService.getCurrentUser()))) {
+        if (!(isUserIsCreatorOfEvent(eventId,currentUser) || checkIfUserRoleIs(currentUser,Role.ADMIN) || userService.getUserById(userId).equals(currentUser))) {
+            MyLogger.logInfo("EXCEPTION: User " + userService.getCurrentUser().getUsername() + " is not authorized to delete user " + userService.getUserById(userId).getUsername() +  " from event " + event.getName());
             throw new NotAuthorizedException("Only creator, admin or user itself can delete users from event");
         }
 
         EventUserStatus eventUserStatus = eventUserStatusRepo.findByEventIdAndUserId(eventId, userId);
 
-        if(eventUserStatus == null)
+        if (eventUserStatus == null) {
+            MyLogger.logInfo("EXCEPTION: User " + userService.getUserById(userId).getUsername() +
+                    " is not registered for event " + event.getName());
             throw new ResourceNotFoundException("User is not registered for this event");
-
+        }
         eventUserStatusRepo.delete(eventUserStatus);
     }
-    public Integer getNumberOfConfirmedUsers(Long eventId){
-         return eventUserStatusRepo.findAllByEventIdAndStatus(eventId, Status.CONFIRMED).size();
+
+    /**
+     * Retrieves the number of users confirmed for a specific event.
+     *
+     * @param eventId the ID of the event
+     * @return the number of confirmed users
+     */
+    public Integer getNumberOfConfirmedUsers(Long eventId) {
+        return eventUserStatusRepo.findAllByEventIdAndStatus(eventId, Status.CONFIRMED).size();
     }
+
+    /**
+     * Retrieves the status of a specific user in an event.
+     *
+     * @param eventId the ID of the event
+     * @param userId the ID of the user
+     * @return the status of the user
+     */
     public Status getEventUserStatus(Long eventId, Long userId) {
         EventUserStatus eventUserStatus = eventUserStatusRepo.findByEventIdAndUserId(eventId, userId);
         return eventUserStatus.getStatus();
     }
+
+    /**
+     * Retrieves all users registered for a specific event as DTOs.
+     *
+     * @param eventId the ID of the event
+     * @return a list of users registered for the event
+     */
     public List<UserOnEvent> getRegisteredUsers(Long eventId) {
-        return eventUserStatusRepo.findAllByEventId(eventId).stream().map(eventUserStatus -> userService.UserToUserOnEvent(userService.getUserById(eventUserStatus.getUserId()), eventId)).toList();
+        return eventUserStatusRepo.findAllByEventId(eventId).stream()
+                .map(eventUserStatus -> userService.UserToUserOnEvent(userService.getUserById(eventUserStatus.getUserId()), eventId))
+                .toList();
     }
-    public List<EventUserStatus> getAllByUserId(Long userId){
+
+    /**
+     * Retrieves all event statuses for a specific user.
+     *
+     * @param userId the ID of the user
+     * @return a list of event statuses
+     */
+    public List<EventUserStatus> getAllByUserId(Long userId) {
         return eventUserStatusRepo.findAllByUserId(userId);
+    }
+
+    public boolean isUserIsCreatorOfEvent(Long eventId, User user) {
+        return eventService.getById(eventId).getCreator().equals(user);
+    }
+
+    public boolean checkIfUserRoleIs(User user, Role role){
+        return user.getRole().equals(role);
     }
 }
