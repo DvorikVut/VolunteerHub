11a3519cf7129d8c52d945157009a25d3150c317
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-12-04 20:13:29 +0100

    swagger-ui api security config change

--- src/main/java/my/pastebin/Config/SecurityConfig.java
+++ src/main/java/my/pastebin/Config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/welcome/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider)
