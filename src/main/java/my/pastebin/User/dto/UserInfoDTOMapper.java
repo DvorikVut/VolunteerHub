a817b95e0df214b688d1c9789876e56e59631697
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-12-04 20:48:52 +0100

    UserInfoDTOMapper added

---
+++ src/main/java/my/pastebin/User/dto/UserInfoDTOMapper.java
@@ -0,0 +1,18 @@
+package my.pastebin.User.dto;
+
+import my.pastebin.User.User;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class UserInfoDTOMapper implements Function<User, UserInfo> {
+    @Override
+    public UserInfo apply(User user) {
+        return UserInfo.builder()
+                .id(user.getId())
+                .surname(user.getSurname())
+                .email(user.getEmail())
+                .build();
+    }
+}
