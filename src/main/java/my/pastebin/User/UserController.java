b9f508251ed5cf18610b56fa02bd81f1e579af00
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-12-04 22:13:34 +0100

    tags for swagger

--- src/main/java/my/pastebin/User/UserController.java
+++ src/main/java/my/pastebin/User/UserController.java
@@ -1,5 +1,7 @@
 package my.pastebin.User;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import my.pastebin.User.dto.UserInfo;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +14,17 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
+@Tag(name = "UserController", description = "Operations related to users")
 public class UserController {
     private final UserService userService;
+
+    @Operation(summary = "Get the profile of the current user")
     @GetMapping("/me")
     public ResponseEntity<UserInfo> getProfile() {
         return userService.getCurrentUserInfo();
     }
 
+    @Operation(summary = "Get top 100 users by points")
     @GetMapping("/top100")
     public ResponseEntity<List<UserInfo>> getTop100UserByPoints(){
         return ResponseEntity.ok(userService.getTop100ByPoints());
