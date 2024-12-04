885eec0569558c7828d09f18504696785a255b7d
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-12-04 22:13:29 +0100

    tags for swagger

--- src/main/java/my/pastebin/Chat/MessageController.java
+++ src/main/java/my/pastebin/Chat/MessageController.java
@@ -1,6 +1,8 @@
 package my.pastebin.Chat;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import my.pastebin.Chat.MessageService;
 import my.pastebin.Chat.dto.NewMessageDTO;
@@ -11,22 +13,30 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/message")
+@Tag(name = "MessageController", description = "Operations related to messages")
 public class MessageController {
     private final MessageService messageService;
+
+    @Operation(summary = "Create a new message")
     @PostMapping
     public ResponseEntity<?> createMessage(@RequestBody NewMessageDTO newMessageDTO){
         messageService.createMessage(newMessageDTO);
         return ResponseEntity.ok("Message was send successfully");
     }
+
+    @Operation(summary = "Get a message by its ID")
     @GetMapping("/{messageId}")
     public ResponseEntity<?> getById(@PathVariable Long messageId){
         return ResponseEntity.ok(messageService.getById(messageId));
     }
+
+    @Operation(summary = "Get all messages sent by a user")
     @GetMapping("/{senderId}")
     public ResponseEntity<?> getBySenderId(@PathVariable Long senderId){
         return ResponseEntity.ok(messageService.getAllBySenderId(senderId));
     }
 
+    @Operation(summary = "Get all messages received by a user")
     @GetMapping("/{receiverId}")
     public ResponseEntity<?> getByReceiverId(@PathVariable Long receiverId){
         return ResponseEntity.ok(messageService.getAllByReceiverId(receiverId));
