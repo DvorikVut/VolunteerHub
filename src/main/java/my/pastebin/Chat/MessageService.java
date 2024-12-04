516b8864f01780ee4a729573bbef76d64e5e1982
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-12-04 22:14:12 +0100

    add sentAt and read in entity

--- src/main/java/my/pastebin/Chat/MessageService.java
+++ src/main/java/my/pastebin/Chat/MessageService.java
@@ -1,31 +1,41 @@
 package my.pastebin.Chat;
 
 import lombok.RequiredArgsConstructor;
+import my.pastebin.Chat.dto.MessageInfoDTO;
+import my.pastebin.Chat.dto.MessageInfoDTOMapper;
 import my.pastebin.Chat.dto.NewMessageDTO;
 import my.pastebin.Exceptions.ResourceNotFoundException;
 import my.pastebin.User.UserService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MessageService {
     private final MessageRepository messageRepository;
+    private final MessageInfoDTOMapper messageInfoDTOMapper;
     private final UserService userService;
 
     public void createMessage(NewMessageDTO newMessageDTO){
         Message message = Message.builder()
-                .recipientId(newMessageDTO.recipientId())
                 .senderId(newMessageDTO.senderId())
+                .recipientId(newMessageDTO.recipientId())
+                .content(newMessageDTO.content())
+                .sentAt(LocalDateTime.now())
+                .read(false)
                 .build();
         save(message);
     }
 
 
     public Message getById(Long messageId){
-        checkIfExists(messageId);
         return messageRepository.getReferenceById(messageId);
+    }
+
+    public MessageInfoDTO getInfoById(Long messageId){
+        return messageInfoDTOMapper.apply(getById(messageId));
     }
 
     public void save(Message message){
