081a3824c8c37fcb50cd35dd0ad9e7676cae9954
Artem Dvorychanskyi <xdvory00@stud.fit.vutbr.cz>
2024-12-06 15:13:31 +0100

    @Service annotation add

--- src/main/java/my/pastebin/Feedback/dto/FeedbackInfoDTOMapper.java
+++ src/main/java/my/pastebin/Feedback/dto/FeedbackInfoDTOMapper.java
@@ -3,10 +3,12 @@ package my.pastebin.Feedback.dto;
 import lombok.RequiredArgsConstructor;
 import my.pastebin.Feedback.Feedback;
 import my.pastebin.User.dto.UserInfoDTOMapper;
+import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
 @RequiredArgsConstructor
+@Service
 public class FeedbackInfoDTOMapper implements Function<Feedback,FeedbackInfoDTO> {
     private final UserInfoDTOMapper userInfoDTOMapper;
     @Override
